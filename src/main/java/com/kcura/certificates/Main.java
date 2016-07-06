16a877fb071bca7e00d765d2eac6d63cf639195f
Michael Di Salvo <mdisalvo@contentanalyst.com>
2016-07-06 12:59:49 -0600

    Validation for argument values

--- src/main/java/com/kcura/certificates/Main.java
+++ src/main/java/com/kcura/certificates/Main.java
@@ -2,10 +2,16 @@ package com.kcura.certificates;
 
 import org.apache.commons.cli.*;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Strings.isNullOrEmpty;
+
 public class Main {
+
+    private static final String DEF_ALIAS = "contenanalyst";
 
     private static String certlocation;
 
@@ -18,6 +24,8 @@ public class Main {
     private static String keystorealias;
 
     private static List<Option> optionsList;
+
+    private static File CAAT_INSTALL_DIR;
 
     private static Options options;
 
@@ -54,14 +62,17 @@ public class Main {
     }
 
     public static void main(String...args) {
-        setValuesFromOpts(args);
+        setValuesFromArgs(args);
+        validateValues();
+        setCAATInstallDirFromValues();
+
         // Accept path to cert, path to caat install, keystorepwd for keystore/truststore, keystorepwd, keystorealias
         // Take self-signed cert, package it into PKCS12 format
         // Use the keytool to import into the trust chain ex
         // Update start.ini and ssl.ini files as needed
     }
 
-    private static void setValuesFromOpts(String[] args) {
+    private static void setValuesFromArgs(String[] args) {
         CommandLineParser clp = new DefaultParser();
         try {
             CommandLine cl = clp.parse(options, args);
@@ -74,6 +85,33 @@ public class Main {
         } catch (ParseException e) {
             System.err.println("Parsing failed.  Reason: " + e.getMessage());
         }
+    }
+
+    private static void validateValues() {
+        checkNotNull(certlocation, "A value must be provided for the certificate location.");
+        checkNotNull(caatlocation, "A value must be provided for the CAAT location.");
+        checkNotNull(keystorename, "A value must be provided for the new keystore name.");
+        checkNotNull(keystorepwd, "A value must be provided for the new keystore password.");
+        if (isNullOrEmpty(keystorealias)) {
+            System.out.println("No value was provided for keystore alias, setting to contentanalyst.");
+            keystorealias = DEF_ALIAS;
+        }
+        isValidCAAT(caatlocation);
+    }
+
+    private static void setCAATInstallDirFromValues() {
+        CAAT_INSTALL_DIR = new File(caatlocation);
+    }
+
+    private static boolean isValidCAAT(String path) {
+        checkNotNull(path);
+        File caatInstallDir = new File(path);
+        return (
+                caatInstallDir.exists() &&
+                        caatInstallDir.isDirectory() &&
+                        new File(caatInstallDir, "start.ini").exists()
+
+        );
     }
 
     private static void help() {
